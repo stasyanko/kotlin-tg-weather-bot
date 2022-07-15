1c253b097a983feb2749dde9194585da7cea9369
Stanislav Yankovskiy <s.yankovskiy@lavinmedia.com>
2022-07-15 18:02:48 +0300

    add chatStep processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
--- src/main/kotlin/com/weather_bot/Application.kt
+++ src/main/kotlin/com/weather_bot/Application.kt
@@ -5,14 +5,52 @@ import com.pengrad.telegrambot.UpdatesListener
 import com.pengrad.telegrambot.model.Update
 import com.pengrad.telegrambot.request.SendMessage
 import io.github.cdimascio.dotenv.Dotenv
+import java.util.*
+
+typealias chatId = Long
+typealias stepNumber = Int
+
+enum class StepEnum(val value: stepNumber) {
+    WEATHER_ACTIONS(1),
+    LOCATION(2),
+    TIME(3),
+}
 
 fun main() {
     val dotenv = Dotenv.load()
     val bot = TelegramBot(dotenv["TG_BOT_TOKEN"])
 
+    val chatSteps = Collections.synchronizedMap(mutableMapOf<
+        chatId,
+        StepEnum
+    >())
+
     bot.setUpdatesListener { updates: List<Update?>? ->
-        updates?.forEach {
+        updates?.forEach { it ->
+            //without null checks it does not compile
             val chatId: Long? = it?.message()?.chat()?.id()
+            //let expr allows to do this without if/else
+            val chatStep = chatSteps[chatId].let { stepNumber ->
+                if(stepNumber === null) {
+                    chatSteps[chatId] = StepEnum.WEATHER_ACTIONS
+                }
+
+                chatSteps[chatId]
+            }
+
+            when(chatStep) {
+                StepEnum.WEATHER_ACTIONS -> {
+                    println("WEATHER_ACTIONS")
+                }
+                StepEnum.LOCATION -> {
+                    println("LOCATION")
+                }
+                //TODO: uncomment this to compile successfully
+//                StepEnum.TIME -> {
+//                    println("TIME")
+//                }
+            }
+
             bot.execute(SendMessage(chatId, "Hello!"))
         }
 
