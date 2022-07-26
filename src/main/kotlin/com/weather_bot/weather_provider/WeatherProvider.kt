208699f4163f17ca3c45ee437831830c01c29d44
Stanislav Yankovskiy <s.yankovskiy@lavinmedia.com>
2022-07-26 15:35:31 +0300

    create WeatherProvider

--- src/main/kotlin/com/weather_bot/weather_provider/WeatherProvider.kt
+++ src/main/kotlin/com/weather_bot/weather_provider/WeatherProvider.kt
@@ -19,12 +19,10 @@ class WeatherProviderAdapter(private val openWeatherMapApi: OpenWeatherMapApi): 
         val threeDayForecast = mutableListOf<WeatherItem>()
         val fiveDayForecast = openWeatherMapApi.fiveDayForecast(lat, lon)
         for (threeHourForecast in fiveDayForecast) {
-            val skyConds = threeHourForecast.skyCondCodes.mapNotNull { mapSkyCondition(it) }
-
             threeDayForecast.add(WeatherItem(
                 Instant.ofEpochSecond(threeHourForecast.dateTimeUnix.toLong()),
                 threeHourForecast.temp,
-                skyConds,
+                threeHourForecast.skyCondCodes.mapNotNull { mapSkyCondition(it) },
             ))
         }
         return threeDayForecast
@@ -32,7 +30,7 @@ class WeatherProviderAdapter(private val openWeatherMapApi: OpenWeatherMapApi): 
 
     private fun mapSkyCondition(openWeatherMapSkyCondition: Int): SkyConditionEnum? {
         // can it be more readable?
-        // it is not code, it is plain english!!!
+        // it is not code, it is just plain english!!!
         return when(openWeatherMapSkyCondition) {
             in 200..531 -> {
                 SkyConditionEnum.RAIN
