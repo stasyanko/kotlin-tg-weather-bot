eca4f02386be6c6a608afb49c990c145788bcc24
Stanislav Yankovskiy <s.yankovskiy@lavinmedia.com>
2022-07-26 14:31:33 +0300

    create OpenWeatherMapApi

--- src/main/kotlin/com/weather_bot/weather_provider/OpenWeatherMapApi.kt
+++ src/main/kotlin/com/weather_bot/weather_provider/OpenWeatherMapApi.kt
@@ -7,8 +7,10 @@ import io.ktor.http.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.math.BigDecimal
+import java.math.BigInteger
 
 data class WeatherItem(
+    val dateTimeUnix: BigInteger,
     val temp: Double,
     val pressure: Int,
     val humidity: Int,
@@ -22,22 +24,23 @@ class OpenWeatherMapApi(
 ) {
     suspend fun fiveDayForecast(lat: BigDecimal, lon: BigDecimal): List<WeatherItem> {
         val res = httpClient.get(
-            "https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appId}"
+            "https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appId}&units=metric"
         )
         if(res.status != HttpStatusCode.OK) {
             println(res.status)
             throw Exception("An error occurred");
         }
-        val weatherByDays = JSONObject(res.bodyAsText()).get("list") as JSONArray
+        val fiveDaysWeather = JSONObject(res.bodyAsText()).get("list") as JSONArray
 
         val result = mutableListOf<WeatherItem>()
-        for (day in weatherByDays) {
+        for (day in fiveDaysWeather) {
             val weatherData = day as JSONObject
             val main = weatherData.get("main") as JSONObject
             val wind = weatherData.get("wind") as JSONObject
             val weatherArr = weatherData.get("weather") as JSONArray
 
             result.add(WeatherItem(
+                weatherData.getBigInteger("dt"),
                 main.getDouble("temp"),
                 main.getInt("pressure"),
                 main.getInt("humidity"),
